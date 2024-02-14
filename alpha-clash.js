55cd60a81427e0343dc518daaf1a693060b2a444
mdujjal67 <mdujjal.mm67@gmail.com>
2024-02-15 00:55:38 +0600

    added score section events

--- alpha-clash.js
+++ alpha-clash.js
@@ -7,8 +7,16 @@ function play(){
     const playgroundSection = document.getElementById('playground-section');
     playgroundSection.classList.remove('hidden');
 
+       // hide the score section
+       const finalScoreSection = document.getElementById('score-section');
+       finalScoreSection.classList.add('hidden');
+
+    //    reset score and life
+    setTextElementById('current-score', 0);
+    setTextElementById('current-life', 5);
     continueGame();
 }
+
 function goBack(){
     // hide the playground section
     const homeSection = document.getElementById('playground-section');
@@ -19,8 +27,9 @@ function goBack(){
     playgroundSection.classList.remove('hidden');
 
     // remove alphabet background color
-    removeBackgroundColorById(alphabet);
+    removeBackgroundColorById(alphabet); 
 }
+
 function continueGame(){
     // generate a random alphabet
     const alphabet = getRandomAlphabet();
@@ -40,6 +49,15 @@ function continueGame(){
 document.addEventListener('keyup', function handleKeyboardKeyUpEven(event){
     const playerPressed = event.key;
     console.log('player pressed' ,playerPressed);
+    // stop the ame if player press escape button
+    if(playerPressed === 'Escape'){
+        const playgroundSection = document.getElementById('playground-section');
+        playgroundSection.classList.add('hidden');
+
+        // show the score section
+        const finalScoreSection = document.getElementById('score-section');
+        finalScoreSection.classList.remove('hidden');
+    }
 
     // get expected to press
     const currentAlphabetElement = document.getElementById('current-alphabet');
@@ -50,11 +68,52 @@ document.addEventListener('keyup', function handleKeyboardKeyUpEven(event){
     // check matched or not
     if(playerPressed === expectedAlphabet){
         console.log('you get a point');
+        // get the score
+        // step-1: get the current score
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+        console.log(currentScore);
+
+        // step-2: increase the score by 1
+        const newScore = currentScore + 1;
+
+        // step-3: show the updated score
+        currentScoreElement.innerText = newScore;
+
+        // start a new round
         removeBackgroundColorById(expectedAlphabet);
         continueGame();
     }
     else{
-        console.log('you lost a life')
+        console.log('you lost a life');
+        // get the life
+        // step-1: get the current life
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+        console.log(currentLife);
+
+        // step-2: decrease the life by 1
+        const newLife = currentLife - 1;
+
+        // step-3: show the updated life
+        currentLifeElement.innerText = newLife;
+
+        if(newLife === 0){
+            // hide the playground section
+            const playgroundSection = document.getElementById('playground-section');
+            playgroundSection.classList.add('hidden');
+
+            // show the score section
+            const finalScoreSection = document.getElementById('score-section');
+            finalScoreSection.classList.remove('hidden');
+
+            // clear the last selected alphabet
+            const currentAlphabetText = this.getElementById('current-alphabet');
+            console.log(currentAlphabetText);
+            removeBackgroundColorById(alphabet);
+        }
     }
 
 });
--- utiity.js
+++ utiity.js
@@ -22,3 +22,15 @@ function removeBackgroundColorById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('bg-orange-400');
 }
+
+function setTextElementById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+
+}
+
+function getElementTextById(elementId){
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
+}
